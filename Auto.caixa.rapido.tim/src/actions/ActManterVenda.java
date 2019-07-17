5f17abff74a86521c1393777455ab8a61b69106c
goutembergNapster <52569125+goutembergNapster@users.noreply.github.com>
2019-07-17 15:40:15 -0300

    commite_de_mais_alteracao

--- Auto.caixa.rapido.tim/src/actions/ActManterVenda.java
+++ Auto.caixa.rapido.tim/src/actions/ActManterVenda.java
@@ -4,6 +4,7 @@ import driverHelper.BasePage;
 import objetos.Produto;
 import pageObject.POCart;
 import pageObject.POCart2;
+import pageObject.POProductQuery_COPY;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,28 +14,29 @@ import driverHelper.*;
 public class ActManterVenda extends BasePage {
 	
 	private static POCart2 pocart = new POCart2();
+	private static POProductQuery_COPY poproductQuery_COPY = new POProductQuery_COPY();
 	
 	public static void ProductSearch(String identificadorUnico) {}
 		
 	private WebDriverWait wait = new WebDriverWait(DriverFactory.getDriver(), 60);
 		
 	public static void ConsultarProduto(Produto produto, boolean isPesquisar) {
-		writeInput(campocodigpo, produto.getCodigo());
-		writeInput(campodescricao,produto.getDescricao());
-		writeInput(campoidentificador,produto.getIdentificadorUnico());
+		writeInput(poproductQuery_COPY.CodeProductField(), produto.getCodigo());
+		writeInput(poproductQuery_COPY.input_productSearchDescription(), produto.getDescricao());
+		writeInput(poproductQuery_COPY.input_productSearchUniqueId(),produto.getIdentificadorUnico());
 		if(isPesquisar){
-		clickElement(botaoPesquisar);
+		clickElement(poproductQuery_COPY.button_productSearch());
 		}
 	}
 	public static void AdicionarProdutoAoCarrinho(Produto produto, String referenciaProduto) {
 		
-		clickElement(funcaoSelecionaItemLinhaPorReferencia(referenciaProduto, colunaRetorno));
-		if("mensagemExibidaNoModal".startsWith("mensagemIdentificadorUnico")) {
-		writeInput(campoidentificador, produto.getIdentificadorUnico());
-		clickElement(botaoOkMensagem);
+		//clickElement(funcaoSelecionaItemLinhaPorReferencia(referenciaProduto, /*colunaRetorno*/));
+		if("mensagemExibidaNoModal".startsWith("mensagemIdentificadorUnico"));
+		writeInput(poproductQuery_COPY.input_productSearchUniqueId(), produto.getIdentificadorUnico());
+		//clickElement(poproductQuery_COPY.);
 		}
 		
-	}	
+		
 	public static void RealizarVenda(Produto produto,boolean isPesquisar) {
 		ConsultarProduto(produto, isPesquisar);
 		AdicionarProdutoAoCarrinho(produto, produto.getCodigo());
